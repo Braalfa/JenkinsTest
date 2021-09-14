0b0754ff13e0bc306ce95fdc40c3afdd0370a531
Brayan <brayanleon25@hotmail.com>
2021-09-13 18:17:59 -0600

    123412

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Jenkins 112!
+          Hola Jenkins 1231243!
         </p>
         <a
           className="App-link"
