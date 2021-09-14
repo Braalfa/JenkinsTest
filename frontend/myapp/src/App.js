2e62761605a59e0da856c5333ca59b54ed7627c7
Brayan <brayanleon25@hotmail.com>
2021-09-13 19:33:16 -0600

    el viejo jenkins

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Jenkins 1231243!
+          Hola viejo Jenkins!
         </p>
         <a
           className="App-link"
